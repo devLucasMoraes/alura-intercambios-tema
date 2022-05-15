a9054dba64a5574acaf9e55405744905926a2791
devLucasMoraes <devlucasmoraes@gmai.com>
2022-05-15 17:59:44 -0300

    criado menu no dashboard do WP para posts personalizados

--- functions.php
+++ functions.php
@@ -1,5 +1,18 @@
 <?php
 
+function alura_intercambios_registrando_post_customizado()
+{
+    register_post_type('destinos', array(
+        'labels' => array('name' => 'Destinos'),
+        'public' => true,
+        'menu_position' => 0,
+        'supports' => array('title', 'editor', 'thumbnail'),
+        'menu_icon' => 'dashicons-admin-site'
+    ));
+}
+
+add_action('init', 'alura_intercambios_registrando_post_customizado');
+
 function alura_intercambios_adicionando_recursos_ao_tema()
 {
     add_theme_support('custom-logo');
